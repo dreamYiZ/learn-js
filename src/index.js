66665417e4a23c4ec757d100c69b9b70af499bb2
Rentoo <dreamyiz@outlook.com>
2020-11-16 01:06:47 +0800

    add image loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./style.css";
+import Icon from "./pic.jpeg";
+import printMe from "./print.js";
 
-function component(){
-    const element = document.createElement('div');
-    element.innerHTML = _.join(['hello', 'webpack'], ' ')
-    return element;
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = _.join(["hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  element.appendChild(myIcon);
+  return element;
 }
 
-document.body.appendChild(component());
+function component2() {
+  const element = document.createElement("div");
+  const btn = document.createElement("button");
+  element.innerHTML = _.join(["hello", "webpack"], " ");
+  btn.innerHTML = "Click me and check the console";
+  element.onclick = printMe;
+  element.appendChild(btn);
+  return element;
+}
+document.body.appendChild(component2());
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,32 @@
-const path = require('path')
+const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    plugins: [
-        new CopyPlugin({
-          patterns: [
-            { from: path.resolve(__dirname, 'public'), to: path.resolve(__dirname, 'dist') },
-          ],
-        }),
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, "public"),
+          to: path.resolve(__dirname, "dist"),
+        },
       ],
-}
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.css$/i, use: ["style-loader", "css-loader"] },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
