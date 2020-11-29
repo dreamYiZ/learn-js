1c19ff76858325a2fc0728fb2be022864608a2e4
Rob <dreamYiZ@outlook.com>
2020-11-30 03:45:44 +0800

    -

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 function Foo() {
   this.x = 100;
   this.y = 99;
-  this.show = function () {
-    console.log(this.x, this.y);
-  };
+//   占用内存,绑定到了当前的实例
+//   this.show = function () {
+//     console.log(this.x, this.y);
+//   };
 }
 
-
+// 静态方法
+// 没有为啥，必须这样写
 Foo.prototype.pShow = function () {
     console.log(this.x, this.y);
 }
